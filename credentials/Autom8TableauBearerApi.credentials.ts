3cb848284c9ecffad319164727f55c3efbf7a945
Timothy Vermeiren <timothy.vermeiren@biztory.be>
2026-03-31 09:27:42 +0200

    Add credentials icon

--- credentials/Autom8TableauBearerApi.credentials.ts
+++ credentials/Autom8TableauBearerApi.credentials.ts
@@ -1,9 +1,9 @@
-import type { ICredentialType, INodeProperties } from 'n8n-workflow';
+import type { Icon, ICredentialType, INodeProperties } from 'n8n-workflow';
 
 export class Autom8TableauBearerApi implements ICredentialType {
   name = 'autom8TableauBearerApi';
   displayName = 'Autom8 Tableau – Bearer Token';
-
+  icon: Icon = { light: 'file:../../icons/autom8.svg', dark: 'file:../../icons/autom8.svg' };
   properties: INodeProperties[] = [
     {
       displayName: 'Bearer Token',
