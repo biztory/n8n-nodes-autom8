716d6d7fc55cc545f11ca1c89d015ceaf9059b9d
Timothy Vermeiren <timothy.vermeiren@biztory.be>
2026-03-31 14:41:27 +0200

    Add test for Autom8TableauBearerApi credential

--- credentials/Autom8TableauBearerApi.credentials.ts
+++ credentials/Autom8TableauBearerApi.credentials.ts
@@ -1,11 +1,11 @@
-import type { Icon, ICredentialType, INodeProperties } from 'n8n-workflow';
+import type { Icon, ICredentialType, INodeProperties, ICredentialTestRequest } from 'n8n-workflow';
 
 export class Autom8TableauBearerApi implements ICredentialType {
   name = 'autom8TableauBearerApi';
   displayName = 'Autom8 Tableau – Bearer Token API';
   icon: Icon = { light: 'file:../icons/autom8-light.svg', dark: 'file:../icons/autom8-dark.svg' };
   documentationUrl = "https://biztory.atlassian.net/wiki/spaces/A8/pages/1122336771/Authentication";
-  test = undefined;
+  test = testAutom8TableauBearerCredentials;
   properties: INodeProperties[] = [
     {
       displayName: 'Bearer Token',
@@ -17,3 +17,15 @@ export class Autom8TableauBearerApi implements ICredentialType {
     },
   ];
 }
+
+// Our test request can not _actually_ verify the credentials, as it is a Bearer token that it will _receive_ as a webhook. What we can do instead, is check that the token (or at least part of it, for "security" is legit according to someone else).
+const testAutom8TableauBearerCredentials: ICredentialTestRequest = {
+  request: {
+    baseURL: 'https://httpbin.org',
+    url: '/bearer',
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer {{$credentials.token.substring(0, 10)}}',
+    },
+  },
+};
